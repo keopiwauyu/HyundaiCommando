741ba479cc66c794e0da06c6d261b53ce9bc2a07
커피와 우유 <106326028+keopiwauyu@users.noreply.github.com>
2022-07-10 21:21:07 +0800

    test sub alias

--- tests/HyundaiSubCommandTest.php
+++ tests/HyundaiSubCommandTest.php
@@ -12,19 +12,27 @@ class HyundaiSubCommandTest extends TestCase {
 	public function testExecute() : void {
 		$update = [];
 		$cmd = UpdateArrayOnExecute::makeHyundai($update, false, true);
+		$expected = ["bbb"];
 
 		$cmd->execute(new FakeCommandSender(), "hello", ["bbb"]);
-		$this->assertSame([array_values($cmd->getSubCommands())[0]->getName()], $update);
+		$this->assertSame($expected, $update);
+
+		$cmd->execute(new FakeCommandSender(), "hello", ["bbb"]);
+		$this->assertSame($expected, $update);
 	}
 
 		public function testExecuteRegisterArgs() : void {
 		$update = [];
 		$cmd = UpdateArrayOnExecute::makeHyundai($update, true, true);
-		$args = ["bbb", "true", "3", "1.4587742654465", "world", "-1.4587742654465", ".0", "-.0", "100", "200", "300", "https://youtu.be/Bc8vc8Y_AYw"]; // TODO: test ~~~ in intragrated tesst
-		$cmd->execute(new FakeCommandSender(), "hello", $args);
+		$args = $expected = ["bbb", "true", "3", "1.4587742654465", "world", "-1.4587742654465", ".0", "-.0", "100", "200", "300", "https://youtu.be/Bc8vc8Y_AYw"]; // TODO: test ~~~ in intragrated tesst
+		$expected[6] = $expected[7] = "0";
 
-		$args[6] = $args[7] = "0";
-		$this->assertSame($args, $update);
+		$cmd->execute(new FakeCommandSender(), "hello", $args);
+		$this->assertSame($expected, $update);
+
+		$args[0] = "eee";
+				$cmd->execute(new FakeCommandSender(), "hello", $args);
+		$this->assertSame($expected, $update);
 	}
 
 }
